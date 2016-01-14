f34ea2832886c8c95666f2e6d7e60919506f9b33
rokuden <rokuden@sfc.wide.ad.jp>
2016-01-14 21:37:45 +0900

    Update treetagger1.py

--- treetagger1.py
+++ treetagger1.py
@@ -1,6 +1,6 @@
 import treetaggerwrapper
 tagger = treetaggerwrapper.TreeTagger(TAGLANG='en',TAGDIR='/Users/Rokuden/Downloads/treetagger')
-tags = tagger.TagText(u"A soccer team which has played a game was going home. Unfortunately, since the driver was exhausted, the car crushes on a black luxury car on their way. As opposed to Miura which protected the younger generation and took all the responsibility -- the conditions of the private settlement to which the Lord of a car and gangster Tanioka were sentenced ...")
+tags = tagger.TagText(u"A soccer team which has played a game was going home. Unfortunately, since the driver was exhausted, the car crushes on a black luxury car on their way. As opposed to Miura which protected the younger generation and took all the responsibility -- the conditions of the private settlement to which the owner of a car and gangster Tanioka were sentenced ...")
 for nouns in tags:
     print nouns
 
