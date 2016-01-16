ececdafac72a5c392288e88fcf574947130db097
rokuden <rokuden@sfc.wide.ad.jp>
2016-01-16 16:36:23 +0900

    表示できるようにはなりました

--- treetagger2.py
+++ treetagger2.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import sys
+sys.path.append('/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages')
+
 print 'Content-type: text/html\n'
 print """
     <!DOCTYPE html>
@@ -10,29 +13,35 @@ print """
             </title>
         </head>
         <body>
-            <p>A soccer team which has played a game was going home. Unfortunately, since the driver was exhausted, the car crushes on a black luxury car on their way. As opposed to Miura which protected the younger generation and took all the responsibility -- the conditions of the private settlement to which the owner of a car and gangster Tanioka were sentenced ...
+            <p>A soccer team <u>which</u> has played a game was going home. Unfortunately, since the driver was exhausted, the car crushes on a black luxury car on their way. Against to Miura which protected the younger generation and took all the responsibility -- the conditions of the private settlement to which the owner of a car and gangster Tanioka were sentenced ...
             </p>
         </body>
     </html>
     """
 import treetaggerwrapper
 tagger = treetaggerwrapper.TreeTagger(TAGLANG='en',TAGDIR='/Users/Rokuden/Downloads/treetagger')
-tags = tagger.TagText(u"A soccer team which has played a game was going home. Unfortunately, since the driver was exhausted, the car crushes on a black luxury car on their way. As opposed to Miura which protected the younger generation and took all the responsibility -- the conditions of the private settlement to which the owner of a car and gangster Tanioka were sentenced ...")
+tags = tagger.TagText(u"A soccer team which has played a game was going home. Unfortunately, since the driver was exhausted, the car crushes on a black luxury car on their way. Against Miura which protected the younger generation and took all the responsibility -- the conditions of the private settlement to which the owner of a car and gangster Tanioka were sentenced ...")
 for tag in tags:
-    html_body="""<html><body>tag</body></html>"""
-    #print tag
     tag_split = tag.split("	")
-    #print tag_split
-    #print tag_split[1]
     if tag_split[1] == "NN":
-        print html_body tag　(tag_split[2])
-    #print("NOUN")
+        print "<html><body><p><u>"
+        print (tag_split[0])
+        print "</u></p></body></html>"
     elif tag_split[1] == "NP":
-        print html_body tag　(tag_split[2])
-    #print("PROPER NOUN")
+        print "<html><body><p><u>"
+        print (tag_split[0])
+        print "</u></p></body></html>"
     elif tag_split[1] == "NNS":
-        print html_body tag　(tag_split[2])
+        print "<html><body><p><u>"
+        print (tag_split[0])
+        print "</u></p></body></html>"
     elif tag_split[1] == "NPS":
-        print(tag_split[2])
+        print "<html><body><p>"
+        print (tag_split[0])
+        print "</u></p></body></html><u>"
     else:
-        print html_body tag　(tag_split[2])
+        print "<html><body><p>"
+        print (tag_split[0])
+        print "</p></body></html>"
+
+
